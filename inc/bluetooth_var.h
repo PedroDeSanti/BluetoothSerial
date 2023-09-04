d7b8d7f1ad34b681d897f35b513ab972b97e8e05
PedroDeSanti <phmartinsanti@gmail.com>
2023-09-04 01:14:38 -0300

    🚧  Update BluetoothVar

--- inc/bluetooth_var.h
+++ inc/bluetooth_var.h
@@ -6,6 +6,7 @@
 #include <array>
 #include <map>
 #include "serializable_interface.h"
+#include "data_types.h"
 
 /*****************************************
  * Class Definition
@@ -41,60 +42,55 @@ class BluetoothVar :
 
         BluetoothVar<T> operator /=(T var_value);
 
-        BluetoothVar<T> operator !();
+        bool operator !();
 
-        BluetoothVar<T> operator &&(T var_value);
+        bool operator &&(T var_value);
 
-        BluetoothVar<T> operator ||(T var_value);
+        bool operator ||(T var_value);
 
-        BluetoothVar<T> operator ==(T var_value);
+        bool operator ==(T var_value);
 
-        BluetoothVar<T> operator !=(T var_value);
+        bool operator !=(T var_value);
 
-        BluetoothVar<T> operator >(T var_value);
+        bool operator >(T var_value);
 
-        BluetoothVar<T> operator <(T var_value);
+        bool operator <(T var_value);
 
-        BluetoothVar<T> operator >=(T var_value);
+        bool operator >=(T var_value);
 
-        BluetoothVar<T> operator <=(T var_value);
+        bool operator <=(T var_value);
 
-        BluetoothVar<T> operator =(BluetoothVar<T> var);
+        // BluetoothVar<T> operator =(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator +(BluetoothVar<T> var);
+        // BluetoothVar<T> operator +(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator -(BluetoothVar<T> var);
+        // BluetoothVar<T> operator -(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator +=(BluetoothVar<T> var);
+        // BluetoothVar<T> operator +=(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator -=(BluetoothVar<T> var);
+        // BluetoothVar<T> operator -=(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator *=(BluetoothVar<T> var);
+        // BluetoothVar<T> operator *=(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator /=(BluetoothVar<T> var);
+        // BluetoothVar<T> operator /=(BluetoothVar<T> var);
 
-        BluetoothVar<T> operator %=(BluetoothVar<T> var);
+        // BluetoothVar<T> operator %=(BluetoothVar<T> var);
+
+        uint8_t* serialize();
+
+        IObject deserialize();
+
+        uint8_t get_size();
 
         T get_value();
 
-        void update_var();
-
     private:
-        void add_var_to_packet();
-
-        void update_packet(uint16_t var_id, T var_value);
-
-        T read_from_packet(uint16_t var_id);
-
         std::string var_name;
-        uint16_t var_id;
+        uint16_t id;
         T var_value;
         bool is_read_only;
 
         uint8_t var_size;
-
-        static std::array<BluetoothVar*, 100> bluetooth_var_pool;
-        static std::map<uint16_t, BluetoothVar*> var_id_to_name;
 };
 
 #include "../src/bluetooth_var.cpp"
--- src/bluetooth_var.cpp
+++ src/bluetooth_var.cpp
@@ -2,60 +2,52 @@
 #define __BLUETOOTH_VAR_CPP__
 
 #include "bluetooth_var.h"
+#include "bluetooth_pool.h"
 
 static uint16_t current_var_id = 0;
 
 template <class T>
 BluetoothVar<T>::BluetoothVar(std::string var_name, T var_value) {
-    this->var_id = current_var_id++;
+    this->id = current_var_id++;
     this->var_name = var_name;
     this->var_value = var_value;
     this->is_read_only = false;
 
-    this->var_size = sizeof(T) / sizeof(uint8_t);
+    this->var_size = sizeof(T);
 
-    add_var_to_packet();
+    BluetoothPool::get_instance()->add_var(this->id, this);
 }
 
 template <class T>
 BluetoothVar<T>::BluetoothVar(std::string var_name) {
-    this->var_id = current_var_id++;
+    this->id = current_var_id++;
     this->var_name = var_name;
     this->var_value = 0;
     this->is_read_only = true;
 
-    add_var_to_packet();
+    this->var_size = sizeof(T);
+
+    BluetoothPool::get_instance()->add_var(this->id, this);
+}
+
+template <class T>
+uint8_t* BluetoothVar<T>::serialize() {
+    return (uint8_t*) (&this->var_value);
+}
+
+template <class T>
+IObject BluetoothVar<T>::deserialize() {
+    (void) 0;
+}
+
+template <class T>
+uint8_t BluetoothVar<T>::get_size() {
+    return this->var_size;
 }
 
 template <class T>
 T BluetoothVar<T>::get_value() {
     return this->var_value;
-}
-
-template <class T>
-void BluetoothVar<T>::update_var() {
-    if (this->is_read_only) {
-        update_packet(this->var_id, this->var_value);
-    } else {
-        this->var_value = read_from_packet(this->var_id);
-    }
-}
-
-template <class T>
-void BluetoothVar<T>::add_var_to_packet() {
-    (void) 0;
-}
-
-template <class T>
-void BluetoothVar<T>::update_packet(uint16_t var_id, T var_value) {
-    (void) var_id;
-    (void) var_value;
-}
-
-template <class T>
-T BluetoothVar<T>::read_from_packet(uint16_t var_id) {
-    (void) var_id;
-    return 0;
 }
 
 // Operators override
@@ -126,127 +118,97 @@ BluetoothVar<T> BluetoothVar<T>::operator /=(T var_value) {
     return *this;
 }
 
+template <class T>
+bool BluetoothVar<T>::operator !() {
+    return (!this->var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator &&(T var_value) {
+    return (this->var_value && var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator ||(T var_value) {
+    return (this->var_value || var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator ==(T var_value) {
+    return (this->var_value == var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator !=(T var_value) {
+    return (this->var_value != var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator >(T var_value) {
+    return (this->var_value > var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator <(T var_value) {
+    return (this->var_value < var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator >=(T var_value) {
+    return (this->var_value >= var_value);
+}
+
+template <class T>
+bool BluetoothVar<T>::operator <=(T var_value) {
+    return (this->var_value <= var_value);
+}
+
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator %=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator =(BluetoothVar<T> var) {
+// this->var_value = var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator &=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator +(BluetoothVar<T> var) {
+// this->var_value += var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator |=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator -(BluetoothVar<T> var) {
+// this->var_value -= var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator ^=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator +=(BluetoothVar<T> var) {
+// this->var_value += var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator <<=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator -=(BluetoothVar<T> var) {
+// this->var_value -= var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator >>=(uint16_t var_value){}
+// BluetoothVar<T> BluetoothVar<T>::operator *=(BluetoothVar<T> var) {
+// this->var_value *= var.var_value;
+// return *this;
+// }
 
 // template <class T>
-// BluetoothVar<T> BluetoothVar<T>::operator ~(){}
+// BluetoothVar<T> BluetoothVar<T>::operator /=(BluetoothVar<T> var) {
+// this->var_value /= var.var_value;
+// return *this;
+// }
 
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator !() {
-    !this->var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator &&(T var_value) {
-    this->var_value && var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator ||(T var_value) {
-    this->var_value || var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator ==(T var_value) {
-    this->var_value == var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator !=(T var_value) {
-    this->var_value != var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator >(T var_value) {
-    this->var_value > var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator <(T var_value) {
-    this->var_value < var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator >=(T var_value) {
-    this->var_value >= var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator <=(T var_value) {
-    this->var_value <= var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator =(BluetoothVar<T> var) {
-    this->var_value = var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator +(BluetoothVar<T> var) {
-    this->var_value += var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator -(BluetoothVar<T> var) {
-    this->var_value -= var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator +=(BluetoothVar<T> var) {
-    this->var_value += var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator -=(BluetoothVar<T> var) {
-    this->var_value -= var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator *=(BluetoothVar<T> var) {
-    this->var_value *= var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator /=(BluetoothVar<T> var) {
-    this->var_value /= var.var_value;
-    return *this;
-}
-
-template <class T>
-BluetoothVar<T> BluetoothVar<T>::operator %=(BluetoothVar<T> var) {
-    this->var_value %= var.var_value;
-    return *this;
-}
+// template <class T>
+// BluetoothVar<T> BluetoothVar<T>::operator %=(BluetoothVar<T> var) {
+// this->var_value %= var.var_value;
+// return *this;
+// }
 
 #endif // __BLUETOOTH_VAR_CPP__
