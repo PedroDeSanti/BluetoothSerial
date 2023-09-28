3de88103d1d4112b0e8b243865223d96189287eb
PedroDeSanti <phmartinsanti@gmail.com>
2023-09-28 10:02:36 -0300

    🚧 Comment unused code

--- inc/bluetooth_serial.h
+++ inc/bluetooth_serial.h
@@ -1,43 +1,44 @@
-#ifndef __BLUETOOTH_SERIAL_H__
-#define __BLUETOOTH_SERIAL_H__
+// #ifndef __BLUETOOTH_SERIAL_H__
+// #define __BLUETOOTH_SERIAL_H__
 
-#include <cstdint>
-#include <string>
-#include <map>
-#include "usart.h"
+// #include <cstdint>
+// #include <string>
+// #include <map>
+// #include "usart.h"
 
-#define PACKET_SIZE 64
+// #define PACKET_SIZE 64
 
-/*****************************************
- * Class Definition
- *****************************************/
+/// *****************************************
+// * Class Definition
+// *****************************************/
 
-class BluetoothSerial {
-    public:
-        BluetoothSerial(UART_HandleTypeDef* uart_handle);
+// class BluetoothSerial {
+// public:
+// BluetoothSerial(UART_HandleTypeDef* uart_handle);
 
-        static void uartRxCallbackProxy(UART_HandleTypeDef* huart);
+// static void uartRxCallbackProxy(UART_HandleTypeDef* huart);
 
-    private:
-        void send_map();
+// private:
+// void send_data();
 
-        void update_var();
+// void update_var();
 
-        bool is_map_request();
+// bool is_map_request();
 
-        // void rx_callback(UART_HandleTypeDef* huart);
+//// void rx_callback(UART_HandleTypeDef* huart);
 
-        // static void uartRxCallbackProxy(BluetoothSerial* instance) {
-        // instance->uartRxCallback();
-        // }
+//// static void uartRxCallbackProxy(BluetoothSerial* instance) {
+//// instance->uartRxCallback();
+//// }
 
-        void uartRxCallback();
+//// void uartRxCallback();
 
-        UART_HandleTypeDef* uart_handle;
+// UART_HandleTypeDef* uart_handle;
 
-        uint8_t rx_data[PACKET_SIZE];
+// uint8_t rx_data[PACKET_SIZE];
+// uint8_t tx_data[PACKET_SIZE];
 
-        std::map<uint16_t, std::string> var_id_to_name;
-};
+// std::map<uint16_t, std::string> var_id_to_name;
+// };
 
-#endif // __BLUETOOTH_SERIAL_H__
+// #endif // __BLUETOOTH_SERIAL_H__
--- src/bluetooth_serial.cpp
+++ src/bluetooth_serial.cpp
@@ -1,50 +1,55 @@
-#include "bluetooth_serial.h"
-#include "dma.h"
-#include "usart.h"
+// #include "bluetooth_serial.h"
+// #include "dma.h"
+// #include "usart.h"
 
-BluetoothSerial::BluetoothSerial(UART_HandleTypeDef* uart_handle) {
-    MX_DMA_Init();
-    MX_USART1_UART_Init();
-    HAL_UART_Receive_DMA(uart_handle, this->rx_data, PACKET_SIZE);
+// BluetoothSerial::BluetoothSerial(UART_HandleTypeDef* uart_handle) {
+// MX_DMA_Init();
+// MX_USART1_UART_Init();
 
-    HAL_UART_RegisterCallback(uart_handle, HAL_UART_RX_COMPLETE_CB_ID, uartRxCallbackProxy);
-}
+//// HAL_UART_Receive_DMA(uart_handle, this->rx_data, PACKET_SIZE);
+//// HAL_UART_Transmit_DMA(uart_handle, this->tx_data, PACKET_SIZE);
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
-    (void) huart;
-}
+// HAL_UART_Transmit(uart_handle, this->tx_data, PACKET_SIZE, 1000);
+// HAL_UART_Receive(uart_handle, this->rx_data, PACKET_SIZE, 1000);
 
-// void BluetoothSerial::rx_callback(UART_HandleTypeDef* huart) {
-// (void) huart;
-
-// if (this->is_map_request()) {
-
-// this->send_map();
-// } else {
-// this->update_var();
-// }
+//// HAL_UART_RegisterCallback(uart_handle, HAL_UART_RX_COMPLETE_CB_ID, uartRxCallbackProxy);
 // }
 
-void BluetoothSerial::uartRxCallbackProxy(UART_HandleTypeDef* huart) {
-    (void) huart;
-}
+//// void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
+//// (void) huart;
+//// }
 
-void BluetoothSerial::uartRxCallback() {
-    if (is_map_request()) {
-        send_map();
-    } else {
-        update_var();
-    }
-}
+//// void BluetoothSerial::rx_callback(UART_HandleTypeDef* huart) {
+//// (void) huart;
 
-bool BluetoothSerial::is_map_request() {
-    return this->rx_data[0] == 0x00;
-}
+//// if (this->is_map_request()) {
 
-void BluetoothSerial::send_map() {
-    (void) 0;
-}
+//// this->send_map();
+//// } else {
+//// this->update_var();
+//// }
+//// }
 
-void BluetoothSerial::update_var() {
-    (void) 0;
-}
+//// void BluetoothSerial::uartRxCallbackProxy(UART_HandleTypeDef* huart) {
+//// (void) huart;
+//// }
+
+//// void BluetoothSerial::uartRxCallback() {
+////// if (is_map_request()) {
+////// send_map();
+////// } else {
+////// update_var();
+////// }
+//// }
+
+//// bool BluetoothSerial::is_map_request() {
+//// return this->rx_data[0] == 0x00;
+//// }
+
+//// void BluetoothSerial::send_map() {
+//// (void) 0;
+//// }
+
+//// void BluetoothSerial::update_var() {
+//// (void) 0;
+//// }
